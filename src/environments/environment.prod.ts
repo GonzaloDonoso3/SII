03470f1eb64a342ea9c38b71c95a4251c74d6c13
diego rios rojas <d.riosrojas007@gmail.com>
2021-04-20 17:47:04 -0400

    correcion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://www.imlchile.cl:3010/api',
+  apiUrl: 'https://www.imlchile.cl:3000/api',
   apiRentacarUrl: 'https://www.imlchile.cl:3010/rentacar/',
   indexUrl: "https://www.imlchile.cl:4200",
   pathApiRent: 'https://www.imlchile.cl:3010/',

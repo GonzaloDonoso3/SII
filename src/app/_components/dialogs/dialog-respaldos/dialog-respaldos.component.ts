9f4596a9c956d26ff63f87f9ad58a60060dd7dd6
Jersthone <jerson.guzman.1997@gmail.com>
2021-05-18 18:18:19 -0400

    cambios ingresos importadora

--- src/app/_components/dialogs/dialog-respaldos/dialog-respaldos.component.ts
+++ src/app/_components/dialogs/dialog-respaldos/dialog-respaldos.component.ts
@@ -1,5 +1,3 @@
-import { environment } from '@environments/environment';
-import { AlertHelper } from '@app/_helpers/alert.helper';
 import { Component, Inject, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -21,15 +19,12 @@ export class DialogRespaldosComponent implements OnInit {
   hasBaseDropZoneOver: boolean;
   hasAnotherDropZoneOver: boolean;
   constructor(
-    private alert: AlertHelper,
     public dialogRef: MatDialogRef<DialogRespaldosComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private snackBar: MatSnackBar,
   ) {
-    //IMPORTANTE: en la api Rest proteger ruta de los upload!! estan espuestas!!! 
     this.uploader = new FileUploader({
-      headers: [{ name: 'authorization', value: localStorage.getItem('usertoken') + '' }],
-      url: `${environment.apiUrl}/${this.data.url}`,
+      url: `http://localhost:3000/api/${this.data.url}`,
       itemAlias: 'photo',
     });
     this.uploader.onCompleteItem = (
@@ -38,8 +33,8 @@ export class DialogRespaldosComponent implements OnInit {
       status: any,
       headers: any
     ) => {
+
       if (!response) {
-        this.alert.errorAlert('tenemos problemas para procesar su solicitud, favor contactar equipo de desarrollo');
         this.snackBar.open(`tenemos problemas para procesar su solicitud, favor contactar equipo de desarrollo`, 'cerrar', {
           duration: 5000,
         });
@@ -74,11 +69,6 @@ export class DialogRespaldosComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  }
-
-  subirFiles() {
-    this.alert.loadingAlert();
-    this.uploader.uploadAll()
   }
 
 }
